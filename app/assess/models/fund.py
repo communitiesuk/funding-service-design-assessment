12eaf17e247b6b55e848e8d0db52bb51d3239b7e
harryyo <56394038+harryyo@users.noreply.github.com>
2022-03-15 19:10:42 +0000

    identifier property, setter and deleter replaced with simple required property

--- app/assess/models/fund.py
+++ app/assess/models/fund.py
@@ -2,34 +2,17 @@ from dataclasses import dataclass
 from typing import List
 
 from app.assess.models.round import Round
-from slugify import slugify
 
 
 @dataclass
 class Fund:
     name: str
-    _identifier: str = None
+    identifier: str
     rounds: List[Round] = None
-
-    @property
-    def identifier(self):
-        if self._identifier:
-            return self._identifier
-        return slugify(self.name)
-
-    @identifier.setter
-    def identifier(self, value):
-        self._identifier = value
-
-    @identifier.deleter
-    def identifier(self):
-        del self._identifier
 
     @staticmethod
     def from_json(data: dict):
-        return Fund(
-            name=data.get("fund_name"), _identifier=data.get("fund_id")
-        )
+        return Fund(name=data.get("fund_name"), identifier=data.get("fund_id"))
 
     def add_round(self, fund_round: Round):
         if not self.rounds:
