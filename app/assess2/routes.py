9b3d0c8d4e6245881e0c879aa4e902c317e8ea6f
J <65361824+SentientHousePlant@users.noreply.github.com>
2022-03-17 14:41:04 +0000

    📝 URL Contract fulfilled - added route.

--- app/assess2/routes.py
+++ app/assess2/routes.py
@@ -6,10 +6,10 @@ assessv2_bp = Blueprint(
     "assessv2_bp",
     __name__,
     url_prefix="/new",
-    template_folder="templates",
+    template_folder="assessment_templates",
 )
 
-@assessv2_bp.route("/", methods=["GET"])
-def welcome():
+@assessv2_bp.route("/view_application/<application_id>", methods=["GET"])
+def view_applcation(application_id):
 
-    return render_template("assesser_project_view.html")
+    return render_template("assesser_project_view.html", application_id=application_id)
