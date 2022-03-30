e9d8bf02a492a013ba2b83b9356856c35b37b7fa
Ram <rmn24in@gmail.com>
2022-03-30 11:18:25 +0100

    🎨  status function refactored

--- app/assess/status.py
+++ app/assess/status.py
@@ -1,73 +1,21 @@
-from app.assess.data import APPLICATION_ENDPOINT
-from app.assess.data import get_data
-from app.config import APPLICATION_STORE_API_HOST
-
-STATUS_FUND_ID = "funding-service-design"
-
-
-def get_statuses(application_id):
-    """_summary_: Function is set up to retrive
-    the data from application store with
-    get_data() function.
+def get_status(questions):
+    """_summary_: GIVEN function return a dictionary
+    of statuses.
 
     Args:
-        application_id: Takes an application_id.
+        questions: Takes get_questions() function
 
     Returns:
-        Returns a dictionary of questions & their statuses.
+        Returns dictionary of statuses
     """
-    status_endpoint = APPLICATION_STORE_API_HOST + APPLICATION_ENDPOINT.format(
-        application_id=application_id, fund_id=STATUS_FUND_ID
+    status = {}
+    status["NOT STARTED"] = sum(
+        value == "NOT STARTED" for value in questions.values()
     )
-    api_data = get_data(status_endpoint)
-    if application_id in api_data.get("id"):
-        questions = api_data.get("questions")
-        data = {data.get("question"): data.get("status") for data in questions}
-        return data
-
-
-def get_status_not_completed(statuses):
-    """_summary_: Given function returns the length
-    of status for "IN PROGRESS" or "NOT STARTED"
-     only from all the questions/statuses retrived
-     from get_status_data
-     function.
-
-     Args:
-         statuses: Takes an instance of get_status_data
-         function.
-
-     Returns:
-         Returns questions with status of "IN PROGRESS" or
-          "NOT STARTED".
-    """
-    number_of_questions_not_completed = {
-        question: status
-        for question, status in statuses.items()
-        if (status == "NOT STARTED" or status == "IN PROGRESS")
-    }
-
-    return len(number_of_questions_not_completed)
-
-
-def get_status_completed(statuses):
-    """_summary_:Given function returns the length
-    of status "COMPLETED" only from all the
-     questions/statuses retrived from get_status_data
-    function.
-
-    Args:
-        statuses: Takes an instance of get_status_data
-        function.
-
-    Returns:
-        Returns questions with status of "COMPLETED".
-
-    """
-    number_of_questions_completed = {
-        question: status
-        for question, status in statuses.items()
-        if status == "COMPLETED"
-    }
-
-    return len(number_of_questions_completed)
+    status["IN PROGRESS"] = sum(
+        value == "IN PROGRESS" for value in questions.values()
+    )
+    status["COMPLETED"] = sum(
+        value == "COMPLETED" for value in questions.values()
+    )
+    return status
