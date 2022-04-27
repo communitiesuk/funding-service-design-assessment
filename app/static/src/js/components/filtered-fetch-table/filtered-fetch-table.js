d0ca1122d77f41ee01bece0d9da6749d2612f226
harryyo <56394038+harryyo@users.noreply.github.com>
2022-04-27 18:04:13 +0100

    fsd-application-link class added to landing page application links

--- app/static/src/js/components/filtered-fetch-table/filtered-fetch-table.js
+++ app/static/src/js/components/filtered-fetch-table/filtered-fetch-table.js
@@ -168,7 +168,7 @@ AssessFrontend.FilteredFetchTable.prototype.formatContent = function(col, data) 
 			return `<strong class="govuk-tag govuk-tag--grey">${data[responseKey]}</strong>`
 		}
 	} else if (responseType == "id") {
-		return `<a href="/assess/application/${data[responseKey]}" class="govuk-link">${data[responseKey]}</a>`
+		return `<a href="/assess/application/${data[responseKey]}" class="govuk-link fsd-application-link">${data[responseKey]}</a>`
 	} else {
 		return data[responseKey];
 	}
