3d8932198eb90509c6e2f0b73f444f8ac80d10bc
Ram <rmn24in@gmail.com>
2022-06-24 15:05:25 +0100

    ✨  new route added for text_input_template

--- app/assess/routes.py
+++ app/assess/routes.py
@@ -1,4 +1,5 @@
 from app.assess.data import *
+from app.assess.models.question_field import QuestionField
 from app.config import APPLICATION_STORE_API_HOST_PUBLIC
 from app.config import ASSESSMENT_HUB_ROUTE
 from flask import abort
@@ -138,3 +139,58 @@ def fund_round(fund_id: str, round_id: str):
         abort(404)
 
     return render_template("round.html", fund=fund, round=fund_round)
+
+
+@assess_bp.route("/fragments/text_input")
+def text_input():
+    """
+    Render html page with json of question & answers & page title.
+    """
+
+    input_text_json = {
+        "name": "Digital Form Builder - Runner text-input-form",
+        "questions": [
+            {
+                "question": "First page",
+                "fields": [
+                    {
+                        "key": "oLfixk",
+                        "title": "Your name",
+                        "type": "text",
+                        "answer": "Harry The Great",
+                    },
+                    {
+                        "key": "npenre",
+                        "title": "Your phone number",
+                        "type": "text",
+                        "answer": "01234567890",
+                    },
+                    {
+                        "key": "CAKCpJ",
+                        "title": "Your email address",
+                        "type": "text",
+                        "answer": "example@example.com",
+                    },
+                    {
+                        "key": "gOgMvi",
+                        "title": "Your UK address",
+                        "type": "text",
+                        "answer": "99 evoco, example street, London, UB5 5FF",
+                    },
+                    {
+                        "key": "suITII",
+                        "title": "Assessor comment",
+                        "type": "text",
+                        "answer": "testing text area",
+                    },
+                ],
+            }
+        ],
+        "metadata": {"paymentSkipped": False},
+    }
+    text_template = QuestionField.get_question_fields(input_text_json)
+    page_title = QuestionField.get_page_title(input_text_json)
+
+    return render_template(
+        "text_input.html", page_title=page_title, json=text_template
+    )
