929c217bd5248e2f09e99097ddcf66c6ad0906c9
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2022-07-04 11:33:21 +0100

    initialising logging and added log level config

--- config/envs/unit_test.py
+++ config/envs/unit_test.py
@@ -1,3 +1,5 @@
+import logging
+
 from config.envs.default import DefaultConfig
 from fsd_utils import configclass
 
@@ -5,3 +7,4 @@ from fsd_utils import configclass
 @configclass
 class UnitTestConfig(DefaultConfig):
     DefaultConfig.TALISMAN_SETTINGS["force_https"] = False
+    FSD_LOG_LEVEL = logging.DEBUG
