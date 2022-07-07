99b61f0d109f1a1edf026528f7990580dc0b7fa6
harryyo <56394038+harryyo@users.noreply.github.com>
2022-07-07 10:44:04 +0100

    Improved docstring and error note

--- app/assets.py
+++ app/assets.py
@@ -4,10 +4,29 @@ from webassets.exceptions import BuildError
 
 
 def compile_static_assets(assets, flask_app):
-    """Configure and build asset bundles."""
+    """
+    Configure and build asset bundles.
 
-    # Main asset bundles
-    # Paths are relative to Flask static_folder
+    Paths in each Bundle() instance below
+    are relative to Flask static_folder.
+    This is set in the config/envs/default.py
+    eg.
+    STATIC_FOLDER = "static/dist"
+    STATIC_URL_PATH = "/assets"
+
+    Bundle(contents,{filters,output,extra,...})
+
+    Note: filters (compilers must be installed in the environment)
+
+    example:
+    default_style_bundle = Bundle(
+        "../src/scss/*.scss", <------- These are the file types to match
+        filters="pyscss,cssmin", <---- These are how to process them
+        output="css/main.min.css", <--- This is the single file that is output
+        extra={"rel": "stylesheet/css"}, <--- Extra template tag attributes
+    )
+    """
+
     default_style_bundle = Bundle(
         "../src/scss/*.scss",
         filters="pyscss,cssmin",
@@ -31,8 +50,8 @@ def compile_static_assets(assets, flask_app):
         default_js_bundle.build()
     except BuildError as e:
         raise BuildError(
-            "Nothing to build - tried looking for static files to build in"
-            " paths relative to the STATIC_FOLDER"
+            "Static Files Build Error - tried looking for static files to"
+            " build in paths relative to the STATIC_FOLDER"
             f" ({flask_app.static_folder}). Please check the static folder"
             " exists and expected static files exist at the relative paths,"
             f" defined here in the contents of the bundle object -> {e}."
