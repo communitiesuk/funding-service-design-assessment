0346d03b965422b00213489171200aacf9583fce
Ram <rmn24in@gmail.com>
2022-07-14 13:14:39 +0100

    form for comment box added

---
+++ app/assess/forms/comments_form.py
@@ -0,0 +1,11 @@
+
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField
+from wtforms.validators import length
+
+
+class CommentsForm(FlaskForm):
+    
+        comment = TextAreaField("Comment",validators = [length(max=200)])
+        
+        
