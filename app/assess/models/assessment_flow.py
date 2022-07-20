829ccb758d2b7599fb5f394f5a40b56a74a40e61
Ram <rmn24in@gmail.com>
2022-07-20 16:40:35 +0100

    assessment_flow logics moved to a AssessmentFlow calss

--- app/assess/models/assessment_flow.py
+++ app/assess/models/assessment_flow.py
@@ -5,26 +5,64 @@ from dataclasses import dataclass
 class AssessmentFlow:
     sections: list
     questions: list
-    question: dict
+    question: list
     fields: list
-    title: str
-    answer: str
+    titles: list
+    answers: list
 
     @staticmethod
     def as_json(assessment_json):
         sections = assessment_json["sections"]
-        questions = [section["questions"] for section in sections]
-        # question_titles = [question[0] for question in questions]
+        print("\n"*3)
+        print(f"SECTIONS::::: {sections}")
+        print("\n"*3)
+   
 
-        return questions
+    def get_section_name(assessment_json, page_index):
+        return assessment_json['sections'][page_index] ['section_name']
+    
+    @staticmethod
+    def get_questions(assessment_json, page_index):
+        questions = []
+        sections = assessment_json['sections'][page_index]
+        for section in sections['questions']:
+            questions.append(section)
+        return questions    
+        
+    def get_question(assessment_json, page_index):
+        get_questions = AssessmentFlow.get_questions(assessment_json, page_index)
+        question = []
+        for q in get_questions: 
+            question.append(q['question'])
+        return question
+        
+    
+    @staticmethod
+    def get_fields(assessment_json, page_index):
+        fields = []
+        get_fields = AssessmentFlow.get_questions(assessment_json, page_index)
+        
+        for field in get_fields:
+            fields.append(field['fields'])
+        return fields
+        
+    
+    
+    @staticmethod
+    def get_titles(assessment_json, page_index):
+        pass
+    
+    def get_answers(assessment_json, page_index):
+        pass
     
     
     @staticmethod
     def get_page_index(page_index: str, sections: list) -> int:
-        
+   
         if not page_index or page_index <= str(0):
             page_index = int(0)
-        elif page_index > str(len(sections)):
-            page_index = int(len(sections))
-               
+            
+        elif page_index > str(len(sections)-1):
+            page_index = int(len(sections)-1)
+                         
         return int(page_index)
