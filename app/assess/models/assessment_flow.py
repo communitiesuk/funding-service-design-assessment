0f252bd8dcdb83411d8f0701ccc466bc5ed07871
Ram <rmn24in@gmail.com>
2022-07-22 09:48:47 +0100

    ♻️  refactored process index logic

--- app/assess/models/assessment_flow.py
+++ app/assess/models/assessment_flow.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 
 @dataclass
 class AssessmentFlow:
+    sections: list
     section_name: list
     questions: list
     question: list
@@ -11,6 +12,7 @@ class AssessmentFlow:
     @staticmethod
     def as_json(assessment_json, page_index):
         return AssessmentFlow(
+            sections=AssessmentFlow.get_sections(assessment_json),
             section_name=AssessmentFlow.get_section_name(
                 assessment_json, page_index
             ),
@@ -22,42 +24,42 @@ class AssessmentFlow:
         )
 
     @staticmethod
-    def get_section_name(assessment_json, page_index):
-        return assessment_json["sections"][page_index]["section_name"]
+    def get_sections(data):
+        return data["sections"]
 
     @staticmethod
-    def get_questions(assessment_json, page_index):
+    def get_section_name(data, page_index):
+        return data["sections"][page_index]["section_name"]
+
+    @staticmethod
+    def get_questions(data, page_index):
         questions = []
-        sections = assessment_json["sections"][page_index]
+        sections = data["sections"][page_index]
         for section in sections["questions"]:
             questions.append(section)
         return questions
 
-    def get_question(assessment_json, page_index):
-        get_questions = AssessmentFlow.get_questions(
-            assessment_json, page_index
-        )
+    def get_question(data, page_index):
+        get_questions = AssessmentFlow.get_questions(data, page_index)
         question = []
         for q in get_questions:
             question.append(q["question"])
         return question
 
     @staticmethod
-    def get_fields(assessment_json, page_index):
+    def get_fields(data, page_index):
         fields = []
-        get_fields = AssessmentFlow.get_questions(assessment_json, page_index)
+        get_fields = AssessmentFlow.get_questions(data, page_index)
 
         for field in get_fields:
             fields.append(field["fields"])
         return fields
 
     @staticmethod
-    def get_page_index(page_index: str, sections: list) -> int:
+    def process_index(index: str, data: list) -> int:
+        sections = AssessmentFlow.get_sections(data)
+        if int(index) > int(len(sections) - 1):
+            index = int(len(sections) - 1)
 
-        if not page_index or page_index <= str(0):
-            page_index = int(0)
-
-        elif page_index > str(len(sections) - 1):
-            page_index = int(len(sections) - 1)
-
-        return int(page_index)
+        return int(index)
+    
