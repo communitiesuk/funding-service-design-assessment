6db6948cf03230fc23e8452868fad7c9e7939e6a
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2022-07-27 15:31:22 +0100

    Removed dev host from common config, doesn't need to be there

--- config/envs/development.py
+++ config/envs/development.py
@@ -1,11 +1,10 @@
 from config.envs.default import DefaultConfig
-from fsd_utils import CommonConfig
 from fsd_utils import configclass
 
 
 @configclass
 class DevelopmentConfig(DefaultConfig):
-    ASSESSMENT_STORE_API_HOST = CommonConfig.ASSESSMENT_STORE_API_HOST_DEV
+    ASSESSMENT_STORE_API_HOST = "https://funding-service-design-assessment-store-dev.london.cloudapps.digital"  # noqa
     ASSESSMENT_SCORE_JUST_ENDPOINT = (
         ASSESSMENT_STORE_API_HOST
         + "/assessments/{assessment_id}/sub_criterias/{sub_criteria_id}/scores"
