32dde92d6b413f57c16788b229fc3a1ccbe9bbd3
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2022-10-11 11:04:08 +0100

    removign commented out line

--- config/envs/development.py
+++ config/envs/development.py
@@ -4,7 +4,6 @@ from fsd_utils import configclass
 
 @configclass
 class DevelopmentConfig(DefaultConfig):
-    # ASSESSMENT_STORE_API_HOST = "https://funding-service-design-assessment-store-dev.london.cloudapps.digital"  # noqa
     ASSESSMENT_SCORE_JUST_ENDPOINT = (
         DefaultConfig.ASSESSMENT_STORE_API_HOST
         + "/assessments/{assessment_id}/sub_criterias/{sub_criteria_id}/scores"
