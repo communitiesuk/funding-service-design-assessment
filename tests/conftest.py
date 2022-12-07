469ce9ed63c8ba33b9331b577568414b71183f51
Gio Karunakaran <gio.karunakaran@version1.com>
2022-12-07 10:42:50 +0000

    fs-1968-score-history

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,4 @@
 import multiprocessing
-import platform
 
 import pytest
 from app.create_app import create_app
@@ -8,8 +7,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-if platform.system() == "Darwin":
-    multiprocessing.set_start_method("fork")  # Required on macOSX
+multiprocessing.set_start_method("fork")  # Required on macOSX
 
 
 def post_driver(driver, path, params):
