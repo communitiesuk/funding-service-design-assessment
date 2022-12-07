ed4037c252a51a13fa0020854578ce5f99c001e4
Gio Karunakaran <gio.karunakaran@version1.com>
2022-12-07 16:47:53 +0000

    comment test envvars

--- config/envs/development.py
+++ config/envs/development.py
@@ -21,11 +21,11 @@ class DevelopmentConfig(DefaultConfig):
     # ASSESSMENT_STORE_API_HOST = "https://funding-service-design-assessment-store-dev.london.cloudapps.digital" # noqa
 
     # for local dev/test use with flask run and USE_LOCAL_DATA = False:
-    USE_LOCAL_DATA = False
-    FUND_STORE_API_HOST = (  # change in fund store repo and adjust here accordingly # noqa
-        "http://127.0.0.1:5002"
-    )
-    ASSESSMENT_STORE_API_HOST = (  # change in assessment store repo and adjust here accordingly # noqa
-        "http://127.0.0.1:5001"
-    )
-    ASSESSMENT_SCORES_ENDPOINT = ASSESSMENT_STORE_API_HOST + "/score"
+    # USE_LOCAL_DATA = False
+    # FUND_STORE_API_HOST = (  # change in fund store repo and adjust here accordingly # noqa
+    #     "http://127.0.0.1:5002"
+    # )
+    # ASSESSMENT_STORE_API_HOST = (  # change in assessment store repo and adjust here accordingly # noqa
+    #     "http://127.0.0.1:5001"
+    # )
+    # ASSESSMENT_SCORES_ENDPOINT = ASSESSMENT_STORE_API_HOST + "/score"
