dcb383b281ea9e19c73ed3ca1774dfbcd0fd6576
adamdavies1 <adam.davies@evoco.co.uk>
2022-12-09 15:46:09 +0000

    revert changes

--- config/envs/development.py
+++ config/envs/development.py
@@ -13,7 +13,6 @@ class DevelopmentConfig(DefaultConfig):
     # for local testing with flask run and USE_LOCAL_DATA = True:
     # USE_LOCAL_DATA = True
     FSD_LOG_LEVEL = logging.INFO
-
     # FUND_STORE_API_HOST = "fund_store"
     # ASSESSMENT_STORE_API_HOST = "assessment_store"
     # APPLICATION_STORE_API_HOST = "application_store"
@@ -22,7 +21,7 @@ class DevelopmentConfig(DefaultConfig):
     # ASSESSMENT_STORE_API_HOST = "https://funding-service-design-assessment-store-dev.london.cloudapps.digital" # noqa
 
     # for local dev/test use with flask run and USE_LOCAL_DATA = False:
-    USE_LOCAL_DATA = True
+    USE_LOCAL_DATA = False
     FUND_STORE_API_HOST = (  # change in fund store repo and adjust here accordingly # noqa
         "http://127.0.0.1:5002"
     )
