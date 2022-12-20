177ee1520aa4c29bc6772dcb659080da3e54569f
adamdavies1 <adam.davies@evoco.co.uk>
2022-12-20 11:06:44 +0000

    Remvoing unused config variables

--- config/envs/unit_test.py
+++ config/envs/unit_test.py
@@ -7,14 +7,3 @@ from os import getenv
 class UnitTestConfig(DefaultConfig):
     DefaultConfig.TALISMAN_SETTINGS["force_https"] = False
     USE_LOCAL_DATA = True
-
-    AWS_ACCESS_KEY_ID =" CommonConfig.AWS_ACCESS_KEY_ID"
-
-    """
-    Aws Config
-    """
-
-    AWS_ACCESS_KEY_ID = getenv("AWS_ACCESS_KEY_ID")
-    AWS_SECRET_ACCESS_KEY =  getenv("AWS_SECRET_ACCESS_KEY")
-    AWS_BUCKET_NAME = getenv("AWS_BUCKET_NAME")
-    AWS_REGION = getenv("AWS_REGION")
