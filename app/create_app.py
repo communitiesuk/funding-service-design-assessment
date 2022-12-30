98fbd1d56418bf9a7d7af811d7027880d9fa5661
Gideon Goldberg <1764158+gidsg@users.noreply.github.com>
2022-12-30 15:00:58 +0000

    add sentry integration

--- app/create_app.py
+++ app/create_app.py
@@ -15,6 +15,7 @@ from flask_compress import Compress
 from flask_talisman import Talisman
 from flask_wtf.csrf import CSRFProtect
 from fsd_utils.authentication.decorators import login_requested
+from fsd_utils import init_sentry
 from fsd_utils.healthchecks.checkers import FlaskRunningChecker
 from fsd_utils.healthchecks.healthcheck import Healthcheck
 from fsd_utils.logging import logging
@@ -24,7 +25,7 @@ from jinja2 import PrefixLoader
 
 
 def create_app() -> Flask:
-
+    init_sentry()
     flask_app = Flask("Assessment Frontend")
 
     flask_app.config.from_object("config.Config")
