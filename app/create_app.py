93c0158ed0f4e86f06b6a16c0dc88ca4c0bbd80c
Ram <80714392+ramsharma-prog@users.noreply.github.com>
2023-01-05 21:02:27 +0000

    Register function to be used as filter in Jinja

--- app/create_app.py
+++ app/create_app.py
@@ -6,6 +6,7 @@ from app.assess.views.filters import format_project_ref
 from app.assess.views.filters import remove_dashes_underscores_capitalize
 from app.assess.views.filters import slash_separated_day_month_year
 from app.assess.views.filters import status_to_human
+from app.assess.views.filters import status_to_human, remove_theme_name_score
 from app.assets import compile_static_assets
 from app.auth import auth_protect
 from config import Config
@@ -51,6 +52,7 @@ def create_app() -> Flask:
     flask_app.jinja_env.filters["all_caps_to_human"] = all_caps_to_human
     flask_app.jinja_env.filters["status_to_human"] = status_to_human
     flask_app.jinja_env.filters["datetime_format_24hr"] = datetime_format_24hr
+    flask_app.jinja_env.filters["remove_theme_name_score"] = remove_theme_name_score
     flask_app.jinja_env.filters["format_project_ref"] = format_project_ref
     flask_app.jinja_env.filters[
         "remove_dashes_underscores_capitalize"
