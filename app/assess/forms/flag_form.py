dc89424e10bc082bbaf52ceecd2c08bbe9f439ef
tferns <thomas.ferns@version1.com>
2023-01-06 12:25:48 +0000

    BAU: Adjust "Flag application" button styling
    - Also change to use form errors from flaskwtf form

--- app/assess/forms/flag_form.py
+++ app/assess/forms/flag_form.py
@@ -7,8 +7,18 @@ from wtforms.validators import length
 
 class FlagApplicationForm(FlaskForm):
     reason = TextAreaField(
-        "Reason for flagging", validators=[length(max=10000), DataRequired()]
+        "Reason for flagging",
+        validators=[
+            length(max=10000),
+            DataRequired(
+                message="Provide a reason for flagging this application"
+            ),
+        ],
     )
     section = StringField(
-        "Section to flag", validators=[length(max=10000), DataRequired()]
+        "Section to flag",
+        validators=[
+            length(max=10000),
+            DataRequired(message="Provide which section you are flagging"),
+        ],
     )
