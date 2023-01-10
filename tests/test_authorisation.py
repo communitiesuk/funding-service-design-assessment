f6cea0eabf918ff25aff8f01b971111573e44d49
Adam Wallace <adam.wallace@communities.gov.uk>
2023-01-10 11:48:23 +0000

    removed overly restrictive auth reqs on flag route (#118)
    Bug fix 🐛
    Flag application—Assessors are unable to flag applications

--- tests/test_authorisation.py
+++ tests/test_authorisation.py
@@ -13,7 +13,7 @@ class TestAuthorisation:
         """
         GIVEN an unauthorised user
         WHEN the user tries to access any route
-        THEN the user is redirected to the "/" root
+        THEN the user is redirected to the "/" route
         Args:
             flask_test_client:
 
@@ -33,7 +33,7 @@ class TestAuthorisation:
         """
         GIVEN an unauthorised user
         WHEN the user tries to access any route
-        THEN the user is redirected to the "/" root
+        THEN the user is redirected to the "/" route
         Args:
             flask_test_client:
 
@@ -51,7 +51,7 @@ class TestAuthorisation:
         assert response.status_code == 302
         assert response.location == "/"
 
-    def test_any_unauthorised_visit_to_root_prompts_user_to_sign_in(
+    def test_any_unauthorised_visit_to_route_prompts_user_to_sign_in(
         self, flask_test_client
     ):
         """
@@ -105,7 +105,7 @@ class TestAuthorisation:
     ):
         """
         GIVEN an authorised user
-        WHEN the user tries to access the service "/" root
+        WHEN the user tries to access the service "/" route
         THEN the user is redirected to their dashboard
         Args:
             flask_test_client:
@@ -135,7 +135,7 @@ class TestAuthorisation:
     ):
         """
         GIVEN an authorised user
-        WHEN the user tries to access the service "/" root
+        WHEN the user tries to access the service "/" route
         THEN the user is redirected to their dashboard
         Args:
             flask_test_client:
@@ -167,7 +167,7 @@ class TestAuthorisation:
     ):
         """
         GIVEN an authorised user
-        WHEN the user tries to access the service "/" root
+        WHEN the user tries to access the service "/" route
         THEN the user is redirected to their dashboard
         Args:
             flask_test_client:
@@ -207,7 +207,7 @@ class TestAuthorisation:
     ):
         """
         GIVEN authorised users
-        WHEN the user accesses the service sub_criteria root
+        WHEN the user accesses the service sub_criteria route
         THEN the user sees the appropriate theme and navbar:
             - test commentor role can not see score in navbar
             - test assessor role can see score in navbar
@@ -246,12 +246,13 @@ class TestAuthorisation:
             (test_lead_assessor_claims, True),
         ],
     )
+
     def test_different_user_levels_see_correct_comments_on_sub_criteria_view(
         self, flask_test_client, claim, expect_all_comments_available
     ):
         """
         GIVEN authorised users
-        WHEN the user accesses the service sub_criteria root
+        WHEN the user accesses the service sub_criteria route
         THEN the user sees the appropriate comments based on role permissions:
             - test commenter can not see comment set by assessor
             - test commenter can not see comment set by lead assessor
@@ -286,3 +287,62 @@ class TestAuthorisation:
         else:
             assert b"Gommez Addams" not in response.data
             assert b"Bruce Wayne" not in response.data
+
+    @pytest.mark.parametrize(
+        "claim,expect_flagging_available",
+        [
+            (test_commenter_claims, False),
+            (test_assessor_claims, True),
+            (test_lead_assessor_claims, True),
+        ],
+    )   
+    def test_different_user_levels_have_correct_flagging_permissions(
+        self, flask_test_client, claim, expect_flagging_available, mocker
+    ):
+        """
+        GIVEN authorised users
+        WHEN the user accesses the service flagging route
+        THEN the user sees the appropriate page:
+            - commentor role can not flag
+            - assessor role can flag
+            - lead assessor role can flag
+        Args:
+            flask_test_client:
+
+        Returns:
+
+        """
+        mocker.patch(
+            "app.assess.routes.get_banner_state",
+            return_value={
+                "short_id":"short",
+                "project_name":"name",
+                "funding_amount_requested":10,
+                "workflow_status":"IN_PROGRESS",
+                "fund_id":"funding-service-design"
+            },
+        )
+
+        flask_test_client.set_cookie(
+            "localhost",
+            "fsd_user_token",
+            create_valid_token(claim),
+        )
+
+        if expect_flagging_available:
+            response = flask_test_client.get(
+                "assess/flag/app123",
+                follow_redirects=True,
+            )
+            assert response.status_code == 200
+            assert b"Flag application" in response.data
+        else:
+            try:
+                flask_test_client.get(
+                "assess/flag/app123",
+                follow_redirects=True,
+                )
+            except Exception as e:
+                # Redirect to authenticator
+                assert e.args[0] == 'Following external redirects is not supported.'
+                
