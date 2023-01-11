d45232f3ee9c362c215d467060038b4ac9d9fb0d
tferns <thomas.ferns@version1.com>
2023-01-11 15:55:28 +0000

    BAU: Rename variable instead of mutating

--- app/assess/routes.py
+++ app/assess/routes.py
@@ -234,14 +234,16 @@ def landing():
         Config.COF_FUND_ID, Config.COF_ROUND2_ID
     ).assessment_deadline
 
-    # Updating assessment progress for applications
-    if application_overviews:
-        application_overviews = get_assessment_progress(application_overviews)
+    post_processed_overviews = (
+        get_assessment_progress(application_overviews)
+        if application_overviews
+        else []
+    )
 
     return render_template(
         "assessor_dashboard.html",
         user=g.user,
-        application_overviews=application_overviews,
+        application_overviews=post_processed_overviews,
         assessment_deadline=assessment_deadline,
         query_params=search_params,
         asset_types=asset_types,
