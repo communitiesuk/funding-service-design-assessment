3170b4a810d8db6ffb25eb3f931b8a1d736a13a7
Adam <adam.wallace@communities.gov.uk>
2023-01-12 17:35:56 +0000

    remove TODO

--- app/assess/forms/resolve_flag_form.py
+++ app/assess/forms/resolve_flag_form.py
@@ -2,9 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import TextAreaField, RadioField
 from wtforms.validators import DataRequired
 
-# TODO
-# add data required validators
-
 class ResolveFlagForm(FlaskForm):
     resolution_flag = RadioField(
         "resolve", 
