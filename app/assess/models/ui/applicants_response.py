6b96a340bd26c8d610b8bd94bb66cf2b7eb090ad
tferns <thomas.ferns@version1.com>
2023-03-16 12:42:54 +0000

    BAU: Pre-commit maintenance

--- app/assess/models/ui/applicants_response.py
+++ app/assess/models/ui/applicants_response.py
@@ -132,27 +132,22 @@ class MonetaryKeyValues(ApplicantResponseComponent):
 
 def _ui_component_from_factory(item: dict, application_id: str):
     """
-    :param item: dict
-    A dictionary representing the UI component to create. It must contain the following keys:
-    - presentation_type (str): The type of presentation for the UI component. Can be one
-      of the following values:
-        - "grouped_fields": A group of related key-value pairs, used for financial tables.
-        - "question_heading": A heading for a group of related questions.
-        - "text": A simple text field.
-        - "list": A list of items. (?)
-        - "file": A file field.
-        - "address": An address field.
-    - field_type (str): The type of field for the UI component.
-      Can be one of the following values:
-        - "multilineTextField": A multi-line text field.
-        - "websiteField": A website field.
-        - "datePartsField": A date field.
-        - "emailAddressField": An email address field.
-        - "telephoneNumberField": A telephone number field.
-        - answer (str): The answer for the UI component.
+    :param item: dict A dictionary representing the UI component to create.
+    It must contain the following keys: - presentation_type (str): The type
+    of presentation for the UI component. Can be one of the following
+    values: - "grouped_fields": A group of related key-value pairs, used for
+    financial tables. - "question_heading": A heading for a group of related
+    questions. - "text": A simple text field. - "list": A list of items. (?)
+    - "file": A file field. - "address": An address field. - field_type (
+    str): The type of field for the UI component. Can be one of the
+    following values: - "multilineTextField": A multi-line text field. -
+    "websiteField": A website field. - "datePartsField": A date field. -
+    "emailAddressField": An email address field. - "telephoneNumberField": A
+    telephone number field. - answer (str): The answer for the UI component.
 
-    :return (ApplicantResponseComponent): An instance of the UI component class.
-    An instance of a subclass of UIComponent, representing the UI component for the given item.
+    :return (ApplicantResponseComponent): An instance of the UI component
+    class. An instance of a subclass of UIComponent, representing the UI
+    component for the given item.
     """
     presentation_type = item["presentation_type"]
     field_type = item.get("field_type")
@@ -213,7 +208,7 @@ def _ui_component_from_factory(item: dict, application_id: str):
 
 
 def _convert_heading_description_amount(
-    response: Iterable[dict],
+        response: Iterable[dict],
 ) -> tuple[list[dict], set[str]]:
     """Convert a list of dictionaries representing a response question and
     answers into a tuple of a list of dictionaries representing grouped fields
@@ -256,10 +251,12 @@ def _convert_heading_description_amount(
         # assigning these keys to variables so it's clearer what they represent
         descriptions, amounts = description["answer"], amount["answer"]
 
-        # remove any pound signs from the amounts, so we can convert them to floats
+        # remove any pound signs from the amounts, so we can convert them to
+        # floats
         poundless_amounts = [a.replace("£", "") for a in amounts]
 
-        # convert the descriptions and amounts to the structure of a grouped field
+        # convert the descriptions and amounts to the structure of a grouped
+        # field
         grouped_fields = list(zip(descriptions, map(float, poundless_amounts)))
 
         items.append(
@@ -274,9 +271,9 @@ def _convert_heading_description_amount(
 
 
 def _build_item(
-    question: str,
-    field_id: str,
-    presentation_type: str = "text",
+        question: str,
+        field_id: str,
+        presentation_type: str = "text",
 ) -> dict:
     return {
         "question": question,
@@ -287,7 +284,7 @@ def _build_item(
 
 
 def _get_item_by_presentation_type_index(
-    response: list[dict], presentation_type: str, index: int
+        response: list[dict], presentation_type: str, index: int
 ) -> dict:
     """Gets an item from a response dictionary based on its presentation type
     and index.
@@ -299,7 +296,8 @@ def _get_item_by_presentation_type_index(
 
     :return (dict): The item with the specified presentation type and index.
 
-    :raises ValueError: If the item with the specified presentation type and index
+    :raises ValueError: If the item with the specified presentation type and
+    index
     """
     try:
         return next(
@@ -324,22 +322,22 @@ def _get_item_by_presentation_type_index(
 
 
 def _convert_checkbox_items(
-    response: Iterable[dict],
+        response: Iterable[dict],
 ) -> tuple[list[dict], set[str]]:
-    """Convert checkbox items in the response into a tuple of a list of dictionary
-    items and a set of field IDs.
+    """Convert checkbox items in the response into a tuple of a list of
+    dictionary items and a set of field IDs.
 
-    The list of dictionary items includes a question heading for the checkbox field,
-    and a dictionary item for each selected checkbox option with the label of the
-    selected option and the value "Yes". If no checkboxes are selected, a single
-    dictionary item with the question heading is added to the list with the value
-    "None selected."
+    The list of dictionary items includes a question heading for the
+    checkbox field, and a dictionary item for each selected checkbox option
+    with the label of the selected option and the value "Yes". If no
+    checkboxes are selected, a single dictionary item with the question
+    heading is added to the list with the value "None selected."
 
     :argument response: A list of dictionaries representing a response from the
     assessment store to questions and answers.
 
-    :return (tuple[list[dict], set[str]]): A tuple of a list of dictionary items
-    and a set of field IDs.
+    :return (tuple[list[dict], set[str]]): A tuple of a list of dictionary
+    items and a set of field IDs.
     """
     checkbox_field_ids = {
         item["field_id"]
@@ -398,44 +396,45 @@ def _flatten_field_ids(field_id: str | tuple | list) -> list[str]:
 
 
 def _convert_non_number_grouped_fields(
-    response: Iterable[dict],
+        response: Iterable[dict],
 ) -> tuple[list[dict], set[str]]:
-    """This function takes a response, an iterable of dictionaries representing data
-    fields, and returns a tuple containing a list of dictionaries representing
-    individual text items and a set of field IDs that have been processed. The
-    dictionaries in the list contain data for a single text field.
+    """This function takes a response, an iterable of dictionaries
+    representing data fields, and returns a tuple containing a list of
+    dictionaries representing individual text items and a set of field IDs
+    that have been processed. The dictionaries in the list contain data for
+    a single text field.
 
-    - response (list[dict]): A list of dictionaries representing a response, the dict
-    keys are:
-        - "question": a string representing the question for the field
-        - "field_type": a string representing the type of field, if applicable
-        - "field_id": a string or list of strings representing the field ID(s)
-        - "answer": a string representing the answer for the field
-        - "presentation_type": a string set to "text"
-    The input response should be an iterable of dictionaries with the following keys:
+    - response (list[dict]): A list of dictionaries representing a response,
+    the dict keys are: - "question": a string representing the question for
+    the field - "field_type": a string representing the type of field,
+    if applicable - "field_id": a string or list of strings representing the
+    field ID(s) - "answer": a string representing the answer for the field -
+    "presentation_type": a string set to "text" The input response should be
+    an iterable of dictionaries with the following keys:
 
-    - "presentation_type": a string representing the type of data field, which should
-    be "grouped_fields" for the fields that this function processes
-    - "field_type": a string representing the type of field, which should not be
-    - "numberField" for the fields that this function processes
-    - "question": a list of strings representing the questions for the fields
-    - "field_id": a list of strings or lists of strings representing the field IDs for
-    the fields
-    - "answer": a list of tuples, where each tuple contains a string representing the
-    question for the field and a string representing the answer for the field
+    - "presentation_type": a string representing the type of data field,
+    which should be "grouped_fields" for the fields that this function
+    processes - "field_type": a string representing the type of field,
+    which should not be - "numberField" for the fields that this function
+    processes - "question": a list of strings representing the questions for
+    the fields - "field_id": a list of strings or lists of strings
+    representing the field IDs for the fields - "answer": a list of tuples,
+    where each tuple contains a string representing the question for the
+    field and a string representing the answer for the field
 
     :argument response: An iterable of dictionaries representing data fields.
 
-    :return (tuple[list[dict], set[str]]): A tuple containing a list of dictionaries
-    representing individual text items and a set of field IDs that have been
-    processed.
+    :return (tuple[list[dict], set[str]]): A tuple containing a list of
+    dictionaries representing individual text items and a set of field IDs
+    that have been processed.
     """
     items_to_process = [
         item
         for item in response
         if item["presentation_type"] == "grouped_fields"
-        and item["field_type"]
-        != "numberField"  # we ignore number fields as they are handled separately # noqa
+           and item["field_type"]
+           != "numberField"
+        # we ignore number fields as they are handled separately # noqa
     ]
 
     text_items = []
@@ -447,7 +446,7 @@ def _convert_non_number_grouped_fields(
             continue
 
         for question_answer_tuple, field_id in zip(
-            item["answer"], item["field_id"]
+                item["answer"], item["field_id"]
         ):
             question, answer = question_answer_tuple
             text_items.append(
@@ -475,23 +474,25 @@ def _make_field_ids_hashable(item: dict) -> dict:
 
 
 def create_ui_components(
-    response_with_some_unhashable_fields: list[dict], application_id: str
+        response_with_some_unhashable_fields: list[dict], application_id: str
 ) -> List[ApplicantResponseComponent]:
     """Creates UI components for displaying applicant responses.
 
-    This function takes a list of dictionaries representing applicant responses
-    with some un-hashable fields and returns a list of ApplicantResponseComponent
-    objects. The un-hashable fields are first made hashable by calling
-    _make_field_ids_hashable. Then, the response is processed and grouped into
-    different categories: heading and description with amounts, checkbox items
-    and non-number grouped fields. These categories are then converted into their
-    respective UI components and added to a list of post-processed items. The
-    list of post-processed items is sorted based on the original order of the fields
-    in the response and returned as a list of ApplicantResponseComponent objects.
+    This function takes a list of dictionaries representing applicant
+    responses with some un-hashable fields and returns a list of
+    ApplicantResponseComponent objects. The un-hashable fields are first
+    made hashable by calling _make_field_ids_hashable. Then, the response is
+    processed and grouped into different categories: heading and description
+    with amounts, checkbox items and non-number grouped fields. These
+    categories are then converted into their respective UI components and
+    added to a list of post-processed items. The list of post-processed
+    items is sorted based on the original order of the fields in the
+    response and returned as a list of ApplicantResponseComponent objects.
 
     :argument response_with_some_unhashable_fields: A list of dictionaries
 
-    :return (list[ApplicantResponseComponent]): A list of ApplicantResponseComponent objects
+    :return (list[ApplicantResponseComponent]): A list of
+    ApplicantResponseComponent objects
     """
     response = list(
         map(_make_field_ids_hashable, response_with_some_unhashable_fields)
@@ -513,10 +514,10 @@ def create_ui_components(
         i for i in response if i["field_id"] not in processed_field_ids
     ]
     post_processed_items = (
-        grouped_fields_items
-        + text_items_from_checkbox
-        + text_items_from_grouped_fields
-        + unprocessed_items
+            grouped_fields_items
+            + text_items_from_checkbox
+            + text_items_from_grouped_fields
+            + unprocessed_items
     )
 
     field_ids_in_order = []
