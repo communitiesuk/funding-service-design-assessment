8ac7bf3c30b53cb2b58a54668165dfaec6392be4
RamuniN <navya.ramuni@communities.gov.uk>
2023-04-06 10:36:37 +0100

    remove the hard coding & use form elements

--- app/assess/forms/continue_application_form.py
+++ app/assess/forms/continue_application_form.py
@@ -1,10 +1,16 @@
+from config import Config
 from flask_wtf import FlaskForm
 from wtforms import TextAreaField
-from wtforms.validators import InputRequired
+from wtforms.validators import (InputRequired, length)
 
 
 class ContinueApplicationForm(FlaskForm):
     reason = TextAreaField(
         "reason",
-        validators=[InputRequired()],
+        validators=[
+            length(max=Config.TEXT_AREA_INPUT_MAX_CHARACTERS),
+            InputRequired(
+                message="Provide a reason for continuing assessment"
+            ),
+        ],
     )
