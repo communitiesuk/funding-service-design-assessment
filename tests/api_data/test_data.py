b4868f6b6c02718fc69b6b9e44a3e3637bf67521
tferns <thomas.ferns@version1.com>
2023-06-23 13:49:20 +0100

    FS-2990: Fix unit test

--- tests/api_data/test_data.py
+++ tests/api_data/test_data.py
@@ -409,19 +409,19 @@ mock_api_results = {
         test_user_id_lead_assessor: {
             "user_id": test_user_id_lead_assessor,
             "full_name": "Lead User",
-            "highest_role": "TF_LEAD_ASSESSOR",
+            "highest_role_map": {"TF": "LEAD_ASSESSOR"},
             "email_address": "lead@test.com",
         },
         test_user_id_commenter: {
             "user_id": test_user_id_commenter,
             "full_name": "Commenter User",
-            "highest_role": "TF_COMMENTER",
+            "highest_role_map": {"TF": "COMMENTER"},
             "email_address": "commenter@test.com",
         },
         test_user_id_assessor: {
             "user_id": test_user_id_assessor,
             "full_name": "Commenter User",
-            "highest_role": "TF_ASSESSOR",
+            "highest_role_map": {"TF": "ASSESSOR"},
             "email_address": "assessor@test.com",
         },
     },
