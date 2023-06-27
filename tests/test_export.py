8d21e67a253e09da2acbea5c87933f980761ca58
Ram <80714392+ramsharma-prog@users.noreply.github.com>
2023-06-27 15:00:57 +0100

    removed redundant tests- To be added in the utils  service

--- tests/test_export.py
+++ tests/test_export.py
@@ -10,45 +10,7 @@ from tests.api_data.example_application_json_blob import (
 
 
 class TestExport:
-    def test_extract_q_and_a(self):
-        result = extract_questions_and_answers_from_json_blob(
-            single_application_json_blob
-        )
-        assert (
-            "sample1.doc"
-            == result["upload-business-plan"]["Upload business plan"]
-        )
-        assert (
-            "lots of surveys"
-            == result["feasibility"][
-                "Tell us about the feasibility studies you have carried out"
-                " for your project"
-            ]
-        )
-        assert (
-            "No"
-            == result["feasibility"][
-                "Do you need to do any further feasibility work?"
-            ]
-        )
-        assert (
-            "Yes"
-            == result["declarations"][
-                "Confirm you have considered subsidy control"
-                " and state aid implications for your project,"
-                " and the information you have given us is"
-                " correct"
-            ]
-        )
-
-    def test_generate_text(self):
-        result = generate_text_of_application(
-            test_application_answers, "TEST FUND"
-        )
-        assert "********* TEST FUND" in result
-        assert "Q) Capital funding" in result
-        assert "A) 2300" in result
-
+    
     def test_get_files_for_application_upload_fields(self):
         application_id = "dummy_id"
         short_id = "d_id"
