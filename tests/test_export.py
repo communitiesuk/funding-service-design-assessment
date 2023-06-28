1d3542055532a4f1b1c24d9c61eb5a7302234c29
Ram <80714392+ramsharma-prog@users.noreply.github.com>
2023-06-28 17:49:19 +0100

    added tests for application mapping, formatting

--- tests/test_export.py
+++ tests/test_export.py
@@ -1,7 +1,7 @@
 from unittest import mock
 
 from app.assess.data import get_files_for_application_upload_fields
-from fsd_utils import extract_questions_and_answers_from_json_blob
+from fsd_utils import extract_questions_and_answers
 from fsd_utils import generate_text_of_application
 from tests.api_data.example_application_answers import test_application_answers
 from tests.api_data.example_application_json_blob import (
@@ -10,7 +10,45 @@ from tests.api_data.example_application_json_blob import (
 
 
 class TestExport:
-    
+    def test_extract_q_and_a(self, app):
+        result = extract_questions_and_answers(
+            single_application_json_blob['forms']
+        )
+        assert (
+            "sample1.doc"
+            == result["upload-business-plan"]["Upload business plan"]
+        )
+        assert (
+            "lots of surveys"
+            == result["feasibility"][
+                "Tell us about the feasibility studies you have carried out"
+                " for your project"
+            ]
+        )
+        assert (
+            "No"
+            == result["feasibility"][
+                "Do you need to do any further feasibility work?"
+            ]
+        )
+        assert (
+            "Yes"
+            == result["declarations"][
+                "Confirm you have considered subsidy control"
+                " and state aid implications for your project,"
+                " and the information you have given us is"
+                " correct"
+            ]
+        )
+
+    def test_generate_text(self):
+        result = generate_text_of_application(
+            test_application_answers, "TEST FUND"
+        )
+        assert "********* TEST FUND" in result
+        assert "Q) Capital funding" in result
+        assert "A) 2300" in result
+
     def test_get_files_for_application_upload_fields(self):
         application_id = "dummy_id"
         short_id = "d_id"
