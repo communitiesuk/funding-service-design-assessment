4a530e0fa48269bc96bb1c333d48bff2c845af54
Adam Davies <adam.davies@version1.com>
2023-07-18 16:25:36 +0100

    fixing flawed if

--- app/assess/models/ui/applicants_response.py
+++ app/assess/models/ui/applicants_response.py
@@ -541,7 +541,7 @@ def _convert_checkbox_items(
                 # The if in this statement has been added because the answer value for ChXWIQ is different than the display value.
                 # We should change the form in future versions
                 "question": remove_dashes_underscores_capitalize(answer)
-                if (item["field_id"] != "ChXWIQ" and answer == "none")
+                if (item["field_id"] != "ChXWIQ" and answer != "none")
                 else "None of these",
                 "field_type": item.get("field_type"),
                 "field_id": item["field_id"],
