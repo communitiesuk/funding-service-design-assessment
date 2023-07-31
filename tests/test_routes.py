0f74bf48e77109c1d2b5b3432180e2baf469059f
tferns <117724519+tferns@users.noreply.github.com>
2023-07-31 09:42:30 +0100

    FS-3249 & BAU: Multiple fixes and changes (#356)
    * FS-3249 & BAU: Multiple fixes and changes
    - Metrics will no longer be filtered by the filters, always show all
    - "Show tags" now work with client-side JavaScript, no longer need to "Search"
    - "All uploaded documents" changes to work per round, instead of per fund
    
    * FS-3249: Fix name of minified javascript, "main"
    
    * BAU: Add missing mocks to fix tests
    
    * BAU: Use checkbox state rather than individual details

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -3,8 +3,10 @@ from unittest import mock
 import app
 import pytest
 from app.assess.models.flag_v2 import FlagV2
+from app.assess.models.round import Round
 from bs4 import BeautifulSoup
 from flask import session
+from tests.api_data.test_data import mock_api_results
 from tests.conftest import create_valid_token
 from tests.conftest import fund_specific_claim_map
 from tests.conftest import test_commenter_claims
@@ -58,7 +60,6 @@ class TestRoutes:
                 "search_in": "organisation_name,short_id",
                 "funding_type": "ALL",
                 "status": "ALL",
-                "show_tags": "OFF",
                 "filter_by_tag": "ALL",
             },
         }
@@ -68,9 +69,9 @@ class TestRoutes:
         self,
         request,
         flask_test_client,
-        mock_get_fund,
         mock_get_funds,
         mock_get_round,
+        mock_get_fund,
         mock_get_application_overviews,
         mock_get_assessment_stats,
         mock_get_teams_flag_stats,
@@ -119,7 +120,6 @@ class TestRoutes:
                 "search_in": "project_name,short_id",
                 "asset_type": "ALL",
                 "status": "ALL",
-                "show_tags": "OFF",
                 "filter_by_tag": "ALL",
             },
         }
@@ -195,7 +195,6 @@ class TestRoutes:
                 "search_in": "project_name,short_id",
                 "asset_type": "ALL",
                 "status": "QA_COMPLETE",
-                "show_tags": "OFF",
                 "filter_by_tag": "ALL",
             },
         }
@@ -241,7 +240,6 @@ class TestRoutes:
                 "search_in": "project_name,short_id",
                 "asset_type": "pub",
                 "status": "ALL",
-                "show_tags": "OFF",
                 "filter_by_tag": "ALL",
             },
         }
@@ -287,7 +285,6 @@ class TestRoutes:
                 "search_in": "project_name,short_id",
                 "asset_type": "ALL",
                 "status": "ALL",
-                "show_tags": "OFF",
                 "filter_by_tag": "ALL",
             },
         }
@@ -334,7 +331,6 @@ class TestRoutes:
                 "search_in": "project_name,short_id",
                 "asset_type": "ALL",
                 "status": "ALL",
-                "show_tags": "OFF",
                 "filter_by_tag": "ALL",
             },
         }
@@ -385,7 +381,6 @@ class TestRoutes:
                 "search_in": "project_name,short_id",
                 "asset_type": "ALL",
                 "status": "ALL",
-                "show_tags": "OFF",
                 "filter_by_tag": "ALL",
             },
         }
@@ -673,7 +668,17 @@ class TestRoutes:
         mock_get_qa_complete,
         mock_get_bulk_accounts,
         mock_get_associated_tags_for_application,
+        mocker,
     ):
+        mocker.patch(
+            "app.assess.routes.get_round",
+            return_value=Round.from_dict(
+                mock_api_results[
+                    "fund_store/funds/{fund_id}/rounds/{round_id}"
+                ]
+            ),
+        )
+
         marker = request.node.get_closest_marker("application_id")
         application_id = marker.args[0]
         token = create_valid_token(test_lead_assessor_claims)
@@ -706,7 +711,17 @@ class TestRoutes:
         mock_get_qa_complete,
         mock_get_bulk_accounts,
         mock_get_associated_tags_for_application,
+        mocker,
     ):
+        mocker.patch(
+            "app.assess.routes.get_round",
+            return_value=Round.from_dict(
+                mock_api_results[
+                    "fund_store/funds/{fund_id}/rounds/{round_id}"
+                ]
+            ),
+        )
+
         marker = request.node.get_closest_marker("application_id")
         application_id = marker.args[0]
         token = create_valid_token(test_lead_assessor_claims)
@@ -968,7 +983,17 @@ class TestRoutes:
         mock_get_funds,
         mock_get_application_metadata,
         mock_get_associated_tags_for_application,
+        mocker,
     ):
+        mocker.patch(
+            "app.assess.routes.get_round",
+            return_value=Round.from_dict(
+                mock_api_results[
+                    "fund_store/funds/{fund_id}/rounds/{round_id}"
+                ]
+            ),
+        )
+
         token = create_valid_token(test_lead_assessor_claims)
         flask_test_client.set_cookie("localhost", "fsd_user_token", token)
         application_id = request.node.get_closest_marker(
@@ -996,7 +1021,17 @@ class TestRoutes:
         mock_get_qa_complete,
         mock_get_bulk_accounts,
         mock_get_associated_tags_for_application,
+        mocker,
     ):
+        mocker.patch(
+            "app.assess.routes.get_round",
+            return_value=Round.from_dict(
+                mock_api_results[
+                    "fund_store/funds/{fund_id}/rounds/{round_id}"
+                ]
+            ),
+        )
+
         token = create_valid_token(test_lead_assessor_claims)
         flask_test_client.set_cookie("localhost", "fsd_user_token", token)
 
@@ -1023,7 +1058,6 @@ class TestRoutes:
                 "search_in": "project_name,short_id",
                 "asset_type": "ALL",
                 "status": "ALL",
-                "show_tags": "OFF",
                 "filter_by_tag": "ALL",
             },
         }
@@ -1032,8 +1066,8 @@ class TestRoutes:
         self,
         request,
         flask_test_client,
-        mock_get_fund,
         mock_get_funds,
+        mock_get_fund,
         mock_get_round,
         mock_get_application_overviews,
         mock_get_assessment_stats,
