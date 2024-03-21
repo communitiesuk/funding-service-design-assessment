89417cdb09965f0792cdb279ea7136a630c3ff59
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2024-03-21 10:55:07 +0000

    Removed selenium as not used

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,10 +8,6 @@ from unittest import mock
 import jwt as jwt
 import pytest
 from flask import template_rendered
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 
 from app.blueprints.assessments.models.round_status import RoundStatus
 from app.blueprints.services.models.assessor_task_list import AssessorTaskList
@@ -251,27 +247,6 @@ def flask_test_maintenance_client(request, user_token=None):
             user_token or create_valid_token(),
         )
         yield test_client
-
-
-@pytest.fixture(scope="class")
-def selenium_chrome_driver(request, live_server):
-    """
-    Returns a Selenium Chrome driver as a fixture for testing.
-    using an installed Chromedriver from the .venv chromedriver_py package
-    install location. Accessible with the
-    @pytest.mark.uses_fixture('selenium_chrome_driver')
-    :return: A selenium chrome driver.
-    """
-
-    service_object = Service(ChromeDriverManager().install())
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    # TODO: set chrome_options.binary_location = ...
-    #  (if setting to run in container or on GitHub)
-    chrome_driver = webdriver.Chrome(service=service_object, options=chrome_options)
-    request.cls.driver = chrome_driver
-    yield
-    request.cls.driver.close()
 
 
 @pytest.fixture(scope="function")
