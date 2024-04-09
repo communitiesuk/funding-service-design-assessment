475a661e21f4a8d49e81bdb23cb46a1642ebd71e
tferns <thomas.ferns@version1.com>
2024-04-09 10:31:52 +0100

    BAU: Split form lists generator into separate files

---
+++ app/blueprints/assessments/form_lists_helper.py
@@ -0,0 +1,18 @@
+from app.blueprints.shared.filters import (
+    remove_dashes_underscores_capitalize_keep_uppercase,
+)
+from app.blueprints.assessments.form_lists_generated_translations import (
+    _LIST_TRANSLATIONS_GENERATED,
+)
+
+
+_LIST_TRANSLATIONS = {
+    **_LIST_TRANSLATIONS_GENERATED,
+    "none": "None of these",  # See PR 338 on GitHub.
+}
+
+
+def map_form_json_list_value(value: str):
+    if mapping := _LIST_TRANSLATIONS.get(value.strip()):
+        return remove_dashes_underscores_capitalize_keep_uppercase(mapping)
+    return value
--- app/blueprints/assessments/models/applicants_response.py
+++ app/blueprints/assessments/models/applicants_response.py
@@ -12,7 +12,7 @@ from urllib.parse import quote
 from bs4 import BeautifulSoup
 from flask import url_for
 
-from app.blueprints.assessments.form_lists_translation_helper import (
+from app.blueprints.assessments.form_lists_helper import (
     map_form_json_list_value,
 )
 from app.blueprints.services.aws import list_files_in_folder
