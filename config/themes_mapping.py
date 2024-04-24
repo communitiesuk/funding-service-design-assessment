b5b0c2ea1fa007abec31b7c028e29963fc20c9ac
Ram <rmn24in@gmail.com>
2024-04-24 14:43:37 +0100

    add ordered themes mapping

---
+++ config/themes_mapping.py
@@ -0,0 +1,59 @@
+def ordered_themes(fund_round_short_name):
+    if fund_round_short_name == "COFR4W1":
+        return [
+            "General information",
+            "Activities",
+            "Partnerships",
+            "Contact information",
+            "Previous funding",
+            "Project summary",
+            "Asset ownership",
+            "Asset evidence",
+            "Asset background",
+            "Asset location",
+            "Business plan",
+            "Declarations",
+            "Community use/significance",
+            "Risk and impact of loss",
+            "Engaging the community",
+            "Local support",
+            "Delivering and sustaining benefits",
+            "Benefitting the whole community",
+            "Environmental considerations",
+            "Funding requested",
+            "Feasiblilty",
+            "Risk",
+            "Income & running costs",
+            "Previous experience",
+            "Governance and structures",
+            "Recruitment",
+            "Representing community views",
+            "Accessibility and inclusivity",
+        ]
+    if fund_round_short_name == "COF-EOIR1":
+        return [
+            "Organisation details",
+            "About your asset",
+            "Your funding request",
+            "Development support provider",
+        ]
+        
+    if fund_round_short_name == "COFR3W3":
+        pass
+    if fund_round_short_name == "COFR3W2":
+        return ['community_use', 'risk_loss_impact', 'engaging-the-community', 'local-support', 'delivering_and_sustaining_benefits', 'benefitting_the_whole_community', 'environmental-considerations', 'funding_requested', 'feasiblilty', 'risk', 'income_&_running_costs', 'previous_experience', 'governance_and_structures', 'recruitment', 'representing_community_views', 'accessibility_and_inclusivity', 'general_information', 'activities', 'partnerships', 'contact_information', 'previous_funding', 'project_summary', 'asset_ownership', 'asset_evidence', 'asset_background', 'asset_location', 'business_plan', 'declarations', 'subsidy_control_and_state_aid']
+    if fund_round_short_name == "COFR3W1":   
+        pass
+    if fund_round_short_name == "COFR2W3":
+        pass
+    if fund_round_short_name == "COFR2W2":
+        pass
+    if fund_round_short_name == "EOY":
+        pass
+    if fund_round_short_name == "DPIFR2":
+        pass
+    if fund_round_short_name == "NSTFR2":
+        pass
+    if fund_round_short_name == "CYP":
+        pass
+    
