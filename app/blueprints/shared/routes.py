655f84d88700f9044013b212b827a01ca27e21bf
Hamza Khalid <hamza.khalid@levellingup.gov.uk>
2024-06-24 00:23:48 +0100

    FS-3873:
    - Cookie Policy page added
    - Route to cookie policy page added
    - Cookie banner added with accept and decline options
    - Unused JS functions removed
    - Show all sections and show are being rendred by the govuk frontend in the version being used

--- app/blueprints/shared/routes.py
+++ app/blueprints/shared/routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint
+from flask import current_app
 from flask import render_template
 
 from app.blueprints.services.data_services import get_default_round_data
@@ -14,6 +15,15 @@ def index():
         login_url=Config.SSO_LOGIN_URL,
         logout_url=Config.SSO_LOGOUT_URL,
         assessment_url=Config.ASSESSMENT_HUB_ROUTE + "/assessor_dashboard/",
+    )
+
+
+@shared_bp.route("/cookie_policy", methods=["GET"])
+def cookie_policy():
+    current_app.logger.info("Cookie policy page loaded.")
+    return render_template(
+        "cookie_policy.html",
+        migration_banner_enabled=Config.MIGRATION_BANNER_ENABLED,
     )
 
 
