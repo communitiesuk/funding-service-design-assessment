10979acbc4fa25ad0d15bafa32c88e0dd9979558
Samuel Williams <samuelhwilliams@gmail.com>
2024-11-07 11:15:22 +0000

    Read maintenance mode variables from param store
    This will allow us to turn maintenance mode on/off with a config change
    and re-deploy, rather than with a code change in a PR. It also means
    that we can put individual environments into maintenance mode, rather
    than requiring all envs go into maintenance mode at once.

--- config/envs/default.py
+++ config/envs/default.py
@@ -21,7 +21,7 @@ class DefaultConfig:
     FLASK_ENV = CommonConfig.FLASK_ENV
     TEXT_AREA_INPUT_MAX_CHARACTERS = 10000
     MAINTENANCE_MODE = strtobool(getenv("MAINTENANCE_MODE", "False"))
-    MAINTENANCE_END_TIME = getenv("MAINTENANCE_END_TIME", "18 December 2023 at 03:00pm")
+    MAINTENANCE_END_TIME = getenv("MAINTENANCE_END_TIME", "soon")
 
     # Authentication
     FSD_USER_TOKEN_COOKIE_NAME = "fsd_user_token"
